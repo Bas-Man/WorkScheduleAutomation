9b8ac900f337e43c242275b064c033e7a5846fd5
Adam.S <Bas-Man@users.noreply.github.com>
2024-03-25 09:46:03 +0900

    tests: more unit.js tests

--- src/__test__/units.test.js
+++ src/__test__/units.test.js
@@ -1,6 +1,6 @@
 
 import { contractType } from '../configuration';
-import { isBonusTimeSlot, isStandardLesson } from '../units';
+import { isBonusTimeSlot, isStandardLesson, createDefaultUnit } from '../units';
 
 
 describe('Test lesson type is not standard', () => {
@@ -20,5 +20,13 @@ describe('Timeslot is a bonus lesson', () => {
   it('isBonusTimeSlot("07:00")', () => {
     expect(contractType).toBe('PL');
     expect(isBonusTimeSlot("07:00")).toBe(true);
+  })
+});
+
+describe('Create a default lesson Unit', () => {
+  it('createDefaultUnit()', () => {
+    const unit = createDefaultUnit();
+    expect(unit.zoom).toBe(false);
+    expect(unit.isBonus).toBe(false);
   })
 });
