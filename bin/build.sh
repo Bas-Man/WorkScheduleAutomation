8ef324cee6f1df082c6037148a126731f5348a65
Adam.S <Bas-Man@users.noreply.github.com>
2024-03-29 11:21:43 +0900

    chore(build script): Create script
    Create test script to prepare a distibutable code using clasp
     - remove /* eslint-diable... */ Statements
     - remove import and export statements

--- bin/build.sh
+++ bin/build.sh
@@ -23,20 +23,24 @@ cp ${SOURCE}/${HTMLHOME} ${DEST}
 
 for FULL_PATH_NAME in $(ls ${SOURCE}/*.js); do
 	FILE=$(basename ${FULL_PATH_NAME})
-	egrep -v '(^i|^\/* eslint-disable)' ${FULL_PATH_NAME} |
-		while read LINE; do
-			echo ${LINE} >>${DEST}/${FILE}
-		done
+
+	# Copy files into dist directory
+	cp ${SOURCE}/${FILE} ${DEST}
+
+	# Remove imports
+	sed -i .orig '/^import/d' ${DEST}/${FILE}
+
+	# remove eslint-disable statements
+	sed -i .orig '/^\/\* eslint-disable/d' ${DEST}/${FILE}
+
+	# Remove exports
+	sed -i .orig '/export {/,/}/d' ${DEST}/${FILE}
 
 	# convert `console` to `Logger`
-	sed 's/console\./Logger\./g' ${DEST}/${FILE} >/tmp/temp.txt && mv /tmp/temp.txt ${DEST}/${FILE}
-	# There is an issue with this script. It's adding /Applications/ and other folders. This ia temp fix
-	# The issue is most like the `/* eslint` where the `/*` is being interpreted
-	sed '/^\/Application/d' ${DEST}/${FILE} >/tmp/temp.txt && mv /tmp/temp.txt ${DEST}/${FILE}
-	# Report exports at the end of the file This is used as these may span multiple lines
-	sed '/^export {/,/};/d' ${DEST}/${FILE} >/tmp/temp.txt && mv /tmp/temp.txt ${DEST}/${FILE}
+	sed -i .orig 's/console\./Logger\./g' ${DEST}/${FILE}
 done
 
 # Fix code formatting after striping import, exports and eslint-disable statements
 cd ${DEST}
-npx prettier . --write
+#npx prettier . --write
+rm *.js.orig
