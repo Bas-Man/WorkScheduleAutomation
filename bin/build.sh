1039f3d3b4d232d76fa74addc13b44535ac614c0
Adam.S <Bas-Man@users.noreply.github.com>
2024-03-29 11:45:44 +0900

    chore: improve script customisation

--- bin/build.sh
+++ bin/build.sh
@@ -6,6 +6,7 @@ DEST=$BASEDIR/dist
 
 APPSCRIPT='appsscript.json'
 HTMLHOME='home.html'
+BACKUP_EXT='.backup'
 
 # Make dir if it does not exist.
 mkdir -p ${DEST}
@@ -28,19 +29,19 @@ for FULL_PATH_NAME in $(ls ${SOURCE}/*.js); do
 	cp ${SOURCE}/${FILE} ${DEST}
 
 	# Remove imports
-	sed -i .orig '/^import/d' ${DEST}/${FILE}
+	sed -i ${BACKUP_EXT} '/^import/d' ${DEST}/${FILE}
 
 	# remove eslint-disable statements
-	sed -i .orig '/^\/\* eslint-disable/d' ${DEST}/${FILE}
+	sed -i ${BACKUP_EXT} '/^\/\* eslint-disable/d' ${DEST}/${FILE}
 
 	# Remove exports
-	sed -i .orig '/export {/,/}/d' ${DEST}/${FILE}
+	sed -i ${BACKUP_EXT} '/export {/,/}/d' ${DEST}/${FILE}
 
 	# convert `console` to `Logger`
-	sed -i .orig 's/console\./Logger\./g' ${DEST}/${FILE}
+	sed -i ${BACKUP_EXT} 's/console\./Logger\./g' ${DEST}/${FILE}
 done
 
 # Fix code formatting after striping import, exports and eslint-disable statements
 cd ${DEST}
 #npx prettier . --write
-rm *.js.orig
+rm *.js${BACKUP_EXT}
